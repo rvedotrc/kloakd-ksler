f6517079a63a52f1d344f944847db4323ab92a47
Rachel Evans <rachel@rachelevans.org>
2020-07-18 09:54:37 +0200

    Clip and rotate shape:circle using svg

--- src/components/ImageList/image_icon.tsx
+++ src/components/ImageList/image_icon.tsx
@@ -13,6 +13,8 @@ type State = {
     imageDownloadUrl?: string;
     usingThumbnail?: boolean;
     isMounted: boolean;
+    naturalWidth?: number;
+    naturalHeight?: number;
 };
 
 class ImageIcon extends React.Component<Props, State> {
@@ -53,10 +55,66 @@ class ImageIcon extends React.Component<Props, State> {
     render() {
         if (!this.state) return "...";
 
-        const { imageDownloadUrl, usingThumbnail } = this.state;
+        const { imageDownloadUrl, usingThumbnail, naturalWidth, naturalHeight } = this.state;
         if (!imageDownloadUrl) return "...";
 
-        const transform = `rotate(${1 * (this.props.dbEntry.rotateDegrees || 0)}deg)`;
+        if (!naturalWidth || !naturalHeight) {
+            const img = document.createElement("img");
+            img.addEventListener("load", () => {
+                this.setState({
+                    naturalWidth: img.naturalWidth,
+                    naturalHeight: img.naturalHeight,
+                });
+            });
+            img.setAttribute("src", imageDownloadUrl);
+            return "....";
+        }
+
+        const safeNaturalWidth = naturalWidth as number;
+        const safeNaturalHeight = naturalHeight as number;
+
+        const degreesRotation = 1 * (this.props.dbEntry.rotateDegrees || 0);
+
+        if (this.props.dbEntry.tags.some(tag => tag === 'shape:circle')) {
+            const desiredRadius = 100;
+            const desiredSize = desiredRadius * 2;
+
+            const clipperId = "clipper-" + this.props.sha;
+
+            // Looks horrible though
+            const scaleUp = (safeNaturalWidth < safeNaturalHeight)
+                ? (safeNaturalHeight / safeNaturalWidth)
+                : (safeNaturalWidth / safeNaturalHeight);
+
+            return (
+                <svg
+                    width={desiredSize} height={desiredSize}
+                    viewBox={`-${desiredRadius} -${desiredRadius} ${desiredSize} ${desiredSize}`}
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                >
+                    <defs>
+                        <clipPath id={clipperId}>
+                            <circle cx="0" cy="0" r={desiredRadius}/>
+                        </clipPath>
+                    </defs>
+
+                    <g clipPath={`url(#${clipperId})`}>
+                        <image
+                            href={imageDownloadUrl}
+                            width={safeNaturalWidth}
+                            height={safeNaturalHeight}
+                            transform={`
+                                scale(${scaleUp})
+                                translate(-${safeNaturalWidth/2} -${safeNaturalHeight/2})
+                                rotate(${degreesRotation} ${safeNaturalWidth/2} ${safeNaturalHeight/2})
+                            `}
+                        />
+                    </g>
+                </svg>
+            );
+        }
+
+        const transform = `rotate(${degreesRotation}deg)`;
         const style: React.CSSProperties = { transform };
 
         if (!usingThumbnail) {
